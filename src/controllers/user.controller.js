6a7e3f03b40e3e269aae76ca8b92a39e13371369
WILLOSU <WILLOSU>
2024-03-06 10:31:40 -0300

    middeware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import userService from "../services/user.service.js";
 
+
 async function createUserController(req, res) {
   const { name, username, email, password, avatar, background } = req.body;
  // desemebrando todos os campos , para validar
@@ -17,7 +18,7 @@ async function createUserController(req, res) {
    
     res.status(201).send(token);
   } catch (e) {
-    return res.status(400).send(e.message);
+    return res.status(400).send("Requisição inválida / Bad Request: " + e.message);
   }
 }
 
@@ -30,7 +31,7 @@ async function findAllUserController(req, res) {
   }
 }
 
-async function findUserByIdController(req, res) {
+async function findUserByIdController(req, res) { // verifica apenas se o ID é válido
   try {
     const user = await userService.findUserByIdService(
       req.params.id,
@@ -38,11 +39,11 @@ async function findUserByIdController(req, res) {
     );
     return res.send(user);
   } catch (e) {
-    return res.status(400).send(e.message);
+    return res.status(400).send("Requisição inválida / Bad Request: " + e.message);
   }
 }
 
-async function updateUserController(req, res) {
+async function updateUserController(req, res) {// try cath, tente fazer alguma coisa se não se não me mostre um erro!!
   try {
     const { name, username, email, password, avatar, background } = req.body;
     const { id: userId } = req.params;
@@ -56,7 +57,7 @@ async function updateUserController(req, res) {
 
     return res.send(response);
   } catch (e) {
-    res.status(400).send(e.message);
+    return res.status(400).send("Requisição inválida / Bad Request: " + e.message);
   }
 }
 
